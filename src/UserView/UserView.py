cfe0c6f85d1c376e6f024a36645d7eefe6447ddf
dekarh3 <dekarh3@gmail.com>
2025-05-17 18:59:54 +0400

    1705-fix-initiaization-error

--- src/UserView/UserView.py
+++ src/UserView/UserView.py
@@ -21,13 +21,14 @@ class UserView(UserView_form, UserView_base):
         self.setWindowTitle("Темы и измерения")
 
         self.themes_model = QSqlTableModel()
-        self.themes_model_initialisation()
+        self.themes_model_initialize()
         self.tv_themes.setModel(self.themes_model)
+        self.tv_themes.hideColumn(0)
         self.tv_themes.clicked.connect(self.themes_model_select_row)
         self.theme_id = -1
 
         self.measures_model = QSqlTableModel()
-        self.measures_model_refreshing()
+        self.measures_model_refresh()
         self.tv_measures.setModel(self.measures_model)
         self.tv_measures.clicked.connect(self.measures_model_select_row)
 
@@ -56,46 +57,51 @@ class UserView(UserView_form, UserView_base):
         self.logger.debug("UserView::on_button_clicked - Database dialog terminated")
         self.logger.debug("UserView::on_button_clicked - Exited method")
 
-    def themes_model_initialisation(self):
+    def themes_model_initialize(self):
         self.logger.debug("UserView::initialise_themes_model - Entered method")
         self.themes_model.setQuery(QSqlQuery('SELECT id, name, about FROM themes WHERE deleted == 0'))
         self.themes_model.select()
-        self.themes_model.setHeaderData(1, Qt.Horizontal, "id")
+        self.themes_model.setHeaderData(0, Qt.Horizontal, "id")
         self.themes_model.setHeaderData(1, Qt.Horizontal, "name")
         self.themes_model.setHeaderData(2, Qt.Horizontal, "about")
         self.logger.debug("UserView::initialise_themes_model - Exited method")
 
     def themes_model_select_row(self, index):
+        self.logger.debug("UserView::themes_model_select_row - Exited method")
         self.theme_id = index.model().data(index.model().index(index.row(), 0))
-        self.measures_model_refreshing()
+        self.measures_model_refresh()
+        self.logger.debug("UserView::themes_model_select_row - Exited method")
 
     def measures_model_query(self, theme_id):
-        return QSqlQuery(f'SELECT '
-                            f'sl.name,'
-                            f'm.date,'
-                            f'm.value,'
-                            f'ut.name '
-                         f'FROM '
-                            f'measures AS m '
-                                f'LEFT JOIN souls AS sl ON m.soulId == sl.id '
-                                f'LEFT JOIN unitTypes AS ut ON m.unitTypeId == ut.id '
-                         f'WHERE '
+        return QSqlQuery('SELECT '
+                            'm.id,'
+                            'sl.name,'
+                            'm.date,'
+                            'm.value,'
+                            'ut.name '
+                         'FROM '
+                            'measures AS m '
+                                'LEFT JOIN souls AS sl ON m.soulId == sl.id '
+                                'LEFT JOIN unitTypes AS ut ON m.unitTypeId == ut.id '
+                         'WHERE '
                             f'm.themeId == {theme_id} AND m.deleted == 0')
 
-    def measures_model_refreshing(self):
-        self.logger.debug("UserView::initialise_measures_model - Entered method")
+    def measures_model_refresh(self):
+        self.logger.debug("UserView::measures_model_refresh - Entered method")
         self.measures_model.setQuery(self.measures_model_query(self.theme_id))
         self.measures_model.select()
-        self.measures_model.setHeaderData(0, Qt.Horizontal, "name")
-        self.measures_model.setHeaderData(1, Qt.Horizontal, "date")
-        self.measures_model.setHeaderData(2, Qt.Horizontal, "value")
-        self.measures_model.setHeaderData(3, Qt.Horizontal, "unit")
-        self.logger.debug("UserView::initialise_measures_model - Exited method")
+        self.measures_model.setHeaderData(0, Qt.Horizontal, "id")
+        self.measures_model.setHeaderData(1, Qt.Horizontal, "name")
+        self.measures_model.setHeaderData(2, Qt.Horizontal, "date")
+        self.measures_model.setHeaderData(3, Qt.Horizontal, "value")
+        self.measures_model.setHeaderData(4, Qt.Horizontal, "unit")
+        self.tv_measures.hideColumn(0)
+        self.logger.debug("UserView::measures_model_refresh - Exited method")
 
     def measures_model_select_row(self, i):
-        self.logger.debug("DbManager::add_row - Entered method")
+        self.logger.debug("UserView::measures_model_select_row - Entered method")
         self.delrow = i.row()
-        self.logger.debug("DbManager::add_row - Exited method")
+        self.logger.debug("UserView::measures_model_select_row - Entered method")
 
     def exit_from_window(self):
         self.main_window = UserView()
